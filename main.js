c309230fba03b3c4e7009a8ebf39a2178deb351f
ObaBolaNiyi <obawoleniyi@gmail.com>
2020-03-26 23:04:26 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// let, const
+let name = "ObaBolaNiyi";
+console.log(name);
+
+const courses = "HTML, CSS, JavaScript";
+console.log(courses.split(", "));
+
+for (var x = 3; x < 200; x += 2)
+    console.log(x)
